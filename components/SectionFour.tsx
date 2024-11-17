c5c268190d80adbdf5a27d9bf82b2594374d2afc
anotherITdude <abhi.baidya@gmail.com>
2024-11-17 17:38:00 +0100

    New: section 4 mobile

--- components/SectionFour.tsx
+++ components/SectionFour.tsx
@@ -12,20 +12,21 @@ const SectionFour = () => {
   const t: Translations = locale === "/" ? en : ar;
   return (
     <Section className="pb-6">
-      <div className=" flex md:min-h-[600px] section4 min-h-[500px] relative  ">
+      <div className=" flex flex-col md:min-h-[600px] section4SM md:section4 min-h-[500px] relative  ">
         <div>
           <Image
-            className="absolute max-w-[300px] -bottom-0"
+            className="absolute max-w-[220px] z-10 md:max-w-[300px] 
+            -bottom-10 left-0 md:-bottom-0"
             src={section_four}
             alt="bg"
           />
         </div>
-        <div className="flex w-full justify-end">
+        <div className="flex w-full justify-center text-center md:justify-end">
           <div
-            className={`w-[60%] p-20  text-right  ${
+            className={`w-full md:w-[60%] p-6 md:p-20  text-right  ${
               locale === "/"
                 ? "font-Albus text-5xl "
-                : "font-Zain-Black text-5xl"
+                : "font-Zain-Black text-4xl md:text-5xl"
             }`}
           >
             <p>{t.nationalday1}</p>
